5731cf81bcab266c7708e3f181ccba638e7cbbda
super27036 <144255894+super27036@users.noreply.github.com>
2024-10-09 20:42:49 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.3.102/g' package/base-files/files/bin/config_generate
+# 修改默认 IP 地址
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认主机名
+sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
+
+# 修改默认密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
+# 禁用 IPv6
+sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
+
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
